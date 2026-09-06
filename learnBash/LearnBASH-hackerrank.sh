3165aee722a41ea11a6f4a65df54b3fb16a71c18
Leonhard Satria Suharjo <leonhard.suharjo@gmail.com>
2026-09-06 23:56:26 +0200

    u

--- learnBash/LearnBASH-hackerrank.sh
+++ learnBash/LearnBASH-hackerrank.sh
@@ -1,12 +1,19 @@
 ## Learn BASH script -- hackerrank 
 ## Last update = 6 sept
 
+/* 
+insights from 
+
+*/
+
+
 
 ## q1,, simple echo 
 
 ## q2, simple loops in bash 
     ### task = use FOR loops to display ODD natural number from 1 to 99
 #!/bin/bash 
-for i in $(seq 1 100); 
-    if 
+for (( i=1; i<=99; i+=2))
+do 
+
     
