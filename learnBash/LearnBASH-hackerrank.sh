83924dd8c6f8d50dbcd7da1a51192ce7c3964dae
Leonhard Satria Suharjo <leonhard.suharjo@gmail.com>
2026-09-07 00:13:22 +0200

    u

--- learnBash/LearnBASH-hackerrank.sh
+++ learnBash/LearnBASH-hackerrank.sh
@@ -15,10 +15,13 @@ EOF
 
 ## q2, simple loops in bash 
     ### task = use FOR loops to display ODD natural number from 1 to 99
-#!/bin/bash 
-for (( i=1; i<=99; i+=2))
+#!/bin/bash // this line is sort of optional but in hackerrank it wont be considered 
+for (( i=1; i<=99; i+=2)) # format => startPosition[i], conditionOf[i], increment[i] 
 do 
     echo $i 
 done 
 
+## q3 -- accepts "name" as input and displays the greeting "Welcome (name)"
+read name 
+echo "Welcome $name"
     
