c18f0830d04b99f8dc93d074934d85fcc462ccc0
Leonhard Satria Suharjo <leonhard.suharjo@gmail.com>
2026-09-06 22:43:45 +0200

    u

--- learnBash/LearnBASH-hackerrank.md
+++
@@ -1,9 +0,0 @@
-Learn BASH script -- hackerrank 
-
-Last update = 6 sept
-
-## q1,, simple echo 
-
-## q2, simple loops in bash 
-task = use FOR loops to display 
-for i in 
---
+++ learnBash/LearnBASH-hackerrank.sh
@@ -0,0 +1,12 @@
+## Learn BASH script -- hackerrank 
+## Last update = 6 sept
+
+
+## q1,, simple echo 
+
+## q2, simple loops in bash 
+    ### task = use FOR loops to display ODD natural number from 1 to 99
+#!/bin/bash 
+for i in $(seq 1 100); 
+    if 
+    
