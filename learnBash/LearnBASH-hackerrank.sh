307b15cbed960783c75b5feed43c5e01ef475ded
Leonhard Satria Suharjo <leonhard.suharjo@gmail.com>
2026-09-08 00:38:31 +0200

    u

--- learnBash/LearnBASH-hackerrank.sh
+++ learnBash/LearnBASH-hackerrank.sh
@@ -8,9 +8,12 @@ insights from learning
 2. always use $[varname] to input a variable to a ouput
     - used for FORMATTED PRINT 
 3. there is NO ";" at the end of line in bash 
-4. use "fi" at the end of if statement 
-5. use "done" at the end of for loop
-6. use esac at the end of 
+4. use "fi" at the end of IF statement 
+    q6
+5. use "done" at the end of FOR loop
+    q4
+6. use esac at the end of CASE
+    q7
 
 -----
 EOF 
